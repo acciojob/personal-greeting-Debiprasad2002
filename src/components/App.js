deedd307b94a53b4d0a5fa04fae16117e189b5e7
Debiprasad Bedanta <113724999+Debiprasad2002@users.noreply.github.com>
2023-12-13 20:43:49 +0000

    first changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleGreet = () => {
+    setGreeting(`Hello ${name}!`);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Your App Title</h1>
+
+      <label>
+        Enter your name:
+        <input type="text" value={name} onChange={handleInputChange} />
+      </label>
+      
+      <button onClick={handleGreet}>Greet</button>
+
+      <div>{greeting}</div>
+
+      {/* Additional components and content can be added here */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
